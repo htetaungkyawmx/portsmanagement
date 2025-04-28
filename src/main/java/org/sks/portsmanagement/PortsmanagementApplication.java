58e8f986d64e4efe06d5aa15b6e8b60d087577a2
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2025-04-28 13:02:49 +0630

    foundation

---
+++ src/main/java/org/sks/portsmanagement/PortsmanagementApplication.java
@@ -0,0 +1,13 @@
+package org.sks.portsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortsmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortsmanagementApplication.class, args);
+    }
+
+}
